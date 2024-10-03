ebb9787579e921cb24895a80c1dbb309cba89c11
QMIER-PC\Qmier <yapy920426@hotmail.com>
2024-10-03 23:02:32 +0800

    調整完框架，但還沒辦法填充到全螢幕

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,192 @@
+<style>
+.ck-editor__editable_inline {
+  min-height: 500px;
+}
+</style>
 <template>
-  <v-layout class="rounded rounded-md">
-    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+  <v-layout ref="app" class="rounded rounded-md" fluid style="height: 100vh;">
+    <v-navigation-drawer color="grey-darken-2" permanent     
+      width="100"
+      name="app-bar2"
+    >
+        <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+        <v-divider></v-divider>
+        <v-list-item link title="List Item 1"></v-list-item>
+        <v-list-item link title="List Item 2"></v-list-item>
+        <v-list-item link title="List Item 3"></v-list-item>
+    </v-navigation-drawer>
 
     <v-app-bar
-      :order="order"
       color="grey-lighten-2"
       title="Application bar"
       flat
+      name="app-bar"
     >
-      <template v-slot:append>
-        <v-switch
-          v-model="order"
-          false-value="0"
-          label="Toggle order"
-          true-value="-1"
-          hide-details
-          inset
-        ></v-switch>
-      </template>
+    
+    <v-btn class="mx-auto" variant="text" @click="print('footer')">
+        Set data
+      </v-btn>
     </v-app-bar>
-
-    <v-main
-      class="d-flex align-center justify-center"
-      style="min-height: 300px"
-    >
-      Main Content
+    
+    <v-main style="display: flex;">
+                      <div id="EditorFive" style="flex: 1; height: 100%; overflow: auto;" >
+                        <ckeditor
+                          v-model="editorData"
+                          :editor="editor"
+                          :config="editorConfig"
+                        ></ckeditor>
+                      </div>
     </v-main>
+
   </v-layout>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      order: 0,
-    }),
-  }
+import { 
+    SourceEditing,
+    Markdown,
+    Autoformat,
+    PasteFromMarkdownExperimental,
+    ClassicEditor,
+    Essentials, 
+    Heading,
+    Bold, 
+    Italic, 
+    Underline,
+    Strikethrough,
+    Code,
+    Subscript,
+    Superscript,
+    Font, 
+    Paragraph,
+    List,
+    ListProperties,
+    TodoList,
+    CodeBlock,
+    BlockQuote,
+    Highlight,
+    Indent, 
+    IndentBlock,
+    RemoveFormat,
+    Clipboard,
+    HorizontalLine,
+    Table,
+    TableToolbar,
+    TableCaption,
+    BlockToolbar,
+    ShowBlocks,
+    Link, 
+    AutoLink,
+    FindAndReplace,
+    HtmlEmbed,
+} from 'ckeditor5';
+import { Ckeditor } from '@ckeditor/ckeditor5-vue';
+import 'ckeditor5/ckeditor5.css';
+export default {
+    methods: {
+      print(key) {
+        alert(JSON.stringify(this.$refs.app.getLayoutItem(key), null, 2))
+        console.log(this)
+      },
+    },
+    name: 'EditorFive',
+    components: {
+        Ckeditor
+    },
+    data() {
+        return {
+            editor: ClassicEditor,
+            editorData: '<p>Hello from CKEditor 5 in Vue!</p>',
+            editorConfig: {
+              height: 500,
+                  plugins: [ 
+                SourceEditing,
+                Markdown,
+                Autoformat,
+                PasteFromMarkdownExperimental,
+                Essentials, 
+                Heading,
+                Bold, 
+                Italic,
+                Underline,
+                Strikethrough,
+                Code,
+                Subscript,
+                Superscript,
+                Font, 
+                Paragraph,            
+                ListProperties,
+                List,
+                TodoList,
+                CodeBlock,
+                BlockQuote,
+                Highlight,
+                Indent, 
+                IndentBlock,
+                RemoveFormat,
+                HorizontalLine,
+                Table,
+                TableToolbar,
+                TableCaption,
+                Clipboard,
+                BlockToolbar,
+                ShowBlocks,
+                Link, 
+                AutoLink,
+                FindAndReplace,
+                HtmlEmbed,
+            ],
+            toolbar: [
+                'undo', 'redo', 'findAndReplace',
+                '|', 
+                'sourceEditing',
+                '|', 
+                'heading',
+                '|',
+                'bold', 'italic', 'strikethrough', 'code',  'blockquote','removeFormat', 'highlight',
+                '|',
+                'bulletedList', 'numberedList', 'todoList', 
+                '|',
+                'link', 'insertTable' , 'codeBlock','horizontalLine',
+                '|',
+                'outdent', 'indent', 
+                '|',
+                'showBlocks', 'htmlEmbed'
+            ],
+            heading: {
+                options: [
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                    { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+                    {
+                        model: 'headingFancy',
+                        view: {
+                            name: 'h2',
+                            classes: 'fancy'
+                        },
+                        title: 'Heading 2 (fancy)',
+                        class: 'ck-heading_heading2_fancy',
+                        converterPriority: 'high'
+                    }
+                ]
+            },            
+            table: {
+                contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ],
+                defaultHeadings: {}
+            },
+            list: {
+                properties: {
+                    styles: false,
+                    startIndex: true
+                }
+            },
+            editorInstance: null // 新增一個屬性來存儲編輯器實例
+            }
+        };
+    }
+};
 </script>
