03ba940c48df16ca032b023549b7e4af6660817e
QMIER-PC\Qmier <yapy920426@hotmail.com>
2024-10-03 14:43:01 +0800

    vuetify的動態布局及順序

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
+  <v-layout class="rounded rounded-md">
+    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+
+    <v-app-bar
+      :order="order"
+      color="grey-lighten-2"
+      title="Application bar"
+      flat
+    >
+      <template v-slot:append>
+        <v-switch
+          v-model="order"
+          false-value="0"
+          label="Toggle order"
+          true-value="-1"
+          hide-details
+          inset
+        ></v-switch>
+      </template>
+    </v-app-bar>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      Main Content
     </v-main>
-  </v-app>
+  </v-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-}
+  export default {
+    data: () => ({
+      order: 0,
+    }),
+  }
 </script>
