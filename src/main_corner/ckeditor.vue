ca9c884cf1c90bb31e88f14bad94b634fb8d7f9d
QMIER-PC\Qmier <yapy920426@hotmail.com>
2024-10-05 20:42:01 +0800

    用ref跟$refs

---
+++ src/main_corner/ckeditor.vue
@@ -0,0 +1,186 @@
+<style>
+.ck-editor:not(.ck-comment__input *) {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  height: 100%;
+}
+.ck-editor__main:not(.ck-comment__input *) {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow-y: auto;
+}
+.ck-editor__editable_inline:not(.ck-comment__input *) {
+  flex: 1; 
+}
+</style>
+<template>
+      <div id="EditorFive" style=" flex: 1; padding: 5px; ">
+        <ckeditor
+          v-model="editorData"
+          :editor="editor"
+          :config="editorConfig"
+          @ready="onEditorReady"
+        ></ckeditor>
+      </div>
+</template>
+
+<script>
+import { 
+    SourceEditing,
+    Markdown,
+    Autoformat,
+    PasteFromMarkdownExperimental,
+    ClassicEditor,
+    Essentials, 
+    Heading,
+    Bold, 
+    Italic, 
+    Underline,
+    Strikethrough,
+    Code,
+    Subscript,
+    Superscript,
+    Font, 
+    Paragraph,
+    List,
+    ListProperties,
+    TodoList,
+    CodeBlock,
+    BlockQuote,
+    Highlight,
+    Indent, 
+    IndentBlock,
+    RemoveFormat,
+    Clipboard,
+    HorizontalLine,
+    Table,
+    TableToolbar,
+    TableCaption,
+    BlockToolbar,
+    ShowBlocks,
+    Link, 
+    AutoLink,
+    FindAndReplace,
+    HtmlEmbed,
+} from 'ckeditor5';
+import { Ckeditor } from '@ckeditor/ckeditor5-vue';
+import 'ckeditor5/ckeditor5.css';
+export default {
+    methods: {
+      onEditorReady(editor) {
+        this.editorInstance = editor; // 存储在组件中
+        window.editor = editor; // 将编辑器实例挂载到全局对象
+        // 這裡可以觸發一個事件，讓父組件知道編輯器已經準備好
+        //this.$emit('editor-ready', editor);//這是反向對上層的@editor-ready="handleEditorReady"執行動作並帶入參數
+      },
+      setEditorData(data) {
+        if (this.editorInstance) {
+          this.editorInstance.setData(data); // 使用编辑器实例的 setData() 方法
+        } else {
+          // 如果编辑器尚未准备就绪，可以选择在 editorData 上直接设置
+          this.editorData = data;
+        }
+      },
+    },
+    name: 'EditorFive',
+    components: {
+        Ckeditor
+    },
+    data() {
+        return {
+            editor: ClassicEditor,
+            editorData: '<p>Hello from CKEditor 5 in Vue!</p>',
+            editorConfig: {
+                  plugins: [ 
+                SourceEditing,
+                Markdown,
+                Autoformat,
+                PasteFromMarkdownExperimental,
+                Essentials, 
+                Heading,
+                Bold, 
+                Italic,
+                Underline,
+                Strikethrough,
+                Code,
+                Subscript,
+                Superscript,
+                Font, 
+                Paragraph,            
+                ListProperties,
+                List,
+                TodoList,
+                CodeBlock,
+                BlockQuote,
+                Highlight,
+                Indent, 
+                IndentBlock,
+                RemoveFormat,
+                HorizontalLine,
+                Table,
+                TableToolbar,
+                TableCaption,
+                Clipboard,
+                BlockToolbar,
+                ShowBlocks,
+                Link, 
+                AutoLink,
+                FindAndReplace,
+                HtmlEmbed,
+            ],
+            toolbar: [
+                'undo', 'redo', 'findAndReplace',
+                '|', 
+                'sourceEditing',
+                '|', 
+                'heading',
+                '|',
+                'bold', 'italic', 'strikethrough', 'code',  'blockquote','removeFormat', 'highlight',
+                '|',
+                'bulletedList', 'numberedList', 'todoList', 
+                '|',
+                'link', 'insertTable' , 'codeBlock','horizontalLine',
+                '|',
+                'outdent', 'indent', 
+                '|',
+                'showBlocks', 'htmlEmbed'
+            ],
+            heading: {
+                options: [
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                    { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+                    {
+                        model: 'headingFancy',
+                        view: {
+                            name: 'h2',
+                            classes: 'fancy'
+                        },
+                        title: 'Heading 2 (fancy)',
+                        class: 'ck-heading_heading2_fancy',
+                        converterPriority: 'high'
+                    }
+                ]
+            },            
+            table: {
+                contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ],
+                defaultHeadings: {}
+            },
+            list: {
+                properties: {
+                    styles: false,
+                    startIndex: true
+                }
+            },
+            editorInstance: null // 新增一個屬性來存儲編輯器實例
+            }
+        };
+    }
+};
+</script>
